106617466fd669521fc0cdd0e844c93baea2ebba
Mayuresh Shelke <163825363+mayuresh-shelke@users.noreply.github.com>
2026-09-05 01:19:49 +0530

    Time: 2 ms (99.31%) | Memory: 44.1 MB (83.11%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -2,17 +2,17 @@ class Solution {
     public boolean isPalindrome(String s) {
         int i=0, e=s.length()-1;
         while(i<e) {
-            if(!Character.isLetterOrDigit(s.charAt(i))) { //Skip if not Alphanumeric
+            if(!Character.isLetterOrDigit(s.charAt(i))) { 
                 i++;
-            }else if(!Character.isLetterOrDigit(s.charAt(e))) { //Skip if not Alphanumeric
+            }else if(!Character.isLetterOrDigit(s.charAt(e))) { 
                 e--;
-            }else if(Character.toLowerCase(s.charAt(i))==Character.toLowerCase(s.charAt(e))) { //Move pointer if both are equal
+            }else if(Character.toLowerCase(s.charAt(i))==Character.toLowerCase(s.charAt(e))) { 
                 i++;
                 e--;
-            }else { //Return false if not equal
+            }else { 
                 return false;
             }
         }
-        return true; //Return true if all pointers passed
+        return true; 
     }
 }
